ae67749384690aca6d6948b5488fe409b353c8b5
Farhan Israq <ishraaq21@gmail.com>
2020-11-05 15:46:23 +0600

    Updated homepage

--- index.php
+++ index.php
@@ -17,12 +17,6 @@
                         <a href="/" class="inline-block py-2 text-gray-800 text-2xl font-bold hover:text-gray-900">Bloodbank.</a>
                     </div>
 
-                    <div>
-                        <div class="hidden md:block">
-                            <a href="#" class="inline-block py-1 md:py-4 text-gray-600 mr-6 font-bold">How it Works</a>
-                        </div>
-                    </div>
-
                     <div class="text-center md:text-right block">
                         <a href="/signin.php" class="inline-block py-1 md:py-4 text-gray-500 hover:text-gray-600 mr-6">Login</a>
                         <a href="/signup.php" class="inline-block py-2 px-4 text-gray-700 bg-white hover:bg-gray-100 rounded-lg">Register</a>
@@ -51,6 +45,12 @@
                     <ul class="flex text-white text-sm flex-wrap items-center">
                         <li class=" text-gray-600">
                             <a href="/search.php" class="p-4 block hover:text-red-500">Search Donors</a>
+                        </li>
+                    </ul>
+
+                    <ul class="flex text-white text-sm flex-wrap items-center">
+                        <li class=" text-gray-600">
+                            <a href="/how-it-works.php" class="p-4 block hover:text-red-500">How It Works?</a>
                         </li>
                     </ul>
 
