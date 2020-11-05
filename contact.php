f5d8a2c754e2eee0dcf50f27d55ba90268e14769
Farhan Israq <ishraaq21@gmail.com>
2020-11-05 16:03:09 +0600

    Created request, contact & how-it-works page

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+require "./includes/Database.php";
+require "./includes/Functions.php";
+
+tryRememberingUser();
+redirectIfNotAuthenticated();
+
+include_once './partials/header.php';
+?>
+
+<main class="px-5 mx-auto my-3 ">
+    <section class="rounded bg-white shadow my-2">
+        <div class="flex items-center justify-center h-screen text-xl">
+            This page is under construction, it'll be live soon.
+        </div>
+    </section>
+</main>
+
+
+<?php
+include_once './partials/footer.php';
+?>
