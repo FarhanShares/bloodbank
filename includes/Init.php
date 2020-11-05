f3c3afb52efe67fcd88e25d8f2fb4cbae29bc1f5
Farhan Israq <ishraaq21@gmail.com>
2020-11-05 17:41:31 +0600

    Session regenerate id

--- includes/Init.php
+++ includes/Init.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_regenerate_id(true);
 
 require_once "Database.php";
 require_once "Functions.php";
--- signout.php
+++ signout.php
@@ -4,8 +4,12 @@ require_once './includes/Init.php';
 redirectIfNotAuthenticated();
 
 unset($_COOKIE['user_id']);
+session_start();
 session_unset();
 session_destroy();
+session_write_close();
+setcookie(session_name(), '', 0, '/');
+session_regenerate_id(true);
 // var_dump(isAuthenticated());
 // tryRememberingUser();
 // var_dump(isAuthenticated());
